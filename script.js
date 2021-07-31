36cdbdcd7457778c85bcb4a533a49d9964b6c8e8
masoomarubab <rubabmasooma26@gmail.com>
2021-08-01 02:46:48 +0500

    Uploading files

---
+++ script.js
@@ -0,0 +1,59 @@
+const calculate=function(data){
+    let totalBill = 0;
+    totalBill =+ document.querySelector('.bill').value;
+    let tip = (totalBill * data)/100;
+    
+    if(document.querySelector('.No-of-people').value == 0 || document.querySelector('.No-of-people').value == NaN ){
+        let a = document.querySelector('.people-span');
+        a.innerHTML = `Can't be zero or empty!`
+        a.classList.add('people-span1')
+    }
+    else{
+        let numberPeople =+ document.querySelector('.No-of-people').value;
+        let tipAmount = (tip/numberPeople);
+
+        let total = totalBill+tip;
+        let totalForPerson = (total / numberPeople);
+
+        let resultAmount = document.querySelector('.tip-total-amount');
+        resultAmount.innerHTML= "$" + totalForPerson.toFixed(2);
+
+        let totalResult = document.querySelector('.tip-total');
+        totalResult.innerHTML= "$" + tipAmount.toFixed(2);
+    }
+}
+
+const btn1=document.querySelector('#btn5');
+btn1.addEventListener('click',function(){
+    calculate(5)
+})
+const btn2=document.querySelector('#btn10');
+btn2.addEventListener('click',function(){
+    calculate(10)
+})
+const btn3=document.querySelector('#btn15');
+btn3.addEventListener('click',function(){
+    calculate(15)
+})
+const btn4=document.querySelector('#btn25');
+btn4.addEventListener('click',function(){
+    calculate(25)
+})
+const btn5=document.querySelector('#btn50');
+btn5.addEventListener('click',function(){
+    calculate(50)
+})
+const customBtn= document.querySelector('.custom-btn');
+customBtn.addEventListener('keypress',function(){
+    let customPer =+ customBtn.value;
+    calculate(customPer); 
+})
+
+let reset = document.querySelector('.reset');
+reset.addEventListener('click',function(){
+    document.querySelector('.bill').value=' ';
+    document.querySelector('.No-of-people').value=' ';
+    document.querySelector('.custom-btn').value=' ';
+    document.querySelector('.tip-total').innerHTML='$0.00';
+    document.querySelector('.tip-total-amount').innerHTML='$0.00';
+})
